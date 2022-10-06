d1c82d9669842577ff977754437cb90973e1d734
AlexandrKryu <a.kryu84@gmail.com>
2022-10-06 14:00:39 +0300

    Коректировочка

--- task6.py
+++ task6.py
@@ -2,12 +2,12 @@
 # использования встроенного генератора псевдослучайных чисел.
 
 import time
-
+size = int(input('Введите длину листа: '))
 def random(n):
     list = []
     for i in range(1, n + 1):
         list.append(round(i * (time.time() * 1000 % 100)))
     return list
 
-list = random(5)
+list = random(size)
 print(list)
